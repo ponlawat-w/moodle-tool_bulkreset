d825fdc6861b0fb126e9403d395727687e831db7
Ponlawat W. <ponlawat_w@outlook.co.th>
2026-01-04 11:45:43 +0700

    Bumped version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_bulkreset';
-$plugin->version = 2025122400;
+$plugin->version = 2026010400;
 $plugin->requires = 2025041400;
-$plugin->release = '1.0.0';
+$plugin->release = '1.0.1';
 $plugin->maturity = MATURITY_STABLE;
